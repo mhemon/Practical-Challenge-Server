16b05a069522b2ad69902e235759e2a8e9fca8a7
mhemon <mhemon02@gmail.com>
2023-07-05 21:17:02 +0600

    jwt routes added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = 5000
 
@@ -10,6 +12,14 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Practical Challenge Accepted...')
+})
+
+//provide jwt token for frontend user
+// this is now publicly accessible but we can took user info from frontend and use post instead of get and pass that into jwt sign rather than a static phone value. in that we can ensure we only provide token for only our frontend calls not any other request.
+
+app.get('/jwt', (req, res) => {
+    const token = jwt.sign({ phone: '0190000' },process.env.ACCESS_TOKEN, { expiresIn: '1h'});
+    res.send({token})
 })
 
 app.listen(port, () => {
